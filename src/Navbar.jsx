1219c89d3f4d8c906d86e90f7f12e27ec94a86b8
Angela Larryant <angelarryant@gmail.com>
2018-11-17 02:32:03 +0000

    made preliminary design

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -7,9 +7,17 @@ class Navbar extends React.Component {
   return (
     <nav>
       <a href="#"><img class="logo" src="/LOGO1-fast.gif" /></a>
-      <div>
-        <a href="/about">About</a>
-        <a href="/contact">Contact</a>
+      <div className="dropdown-container">
+        <div className="dropdown">
+          <button className="dropbtn">Home <i class="fas fa-bars"></i></button>
+          <div className="dropdown-content">
+            <a href="/dashboard">Dashboard</a>
+            <a href="/settings">Settings</a>
+          </div>
+        </div>
+      </div>
+      <div className="userauth">
+        <a href="/login">Login</a> | <a href="/signup">Sign up</a>
       </div>
     </nav>
   )
