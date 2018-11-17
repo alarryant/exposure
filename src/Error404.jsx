32159660aa33f9777ffdb3d9c72a54585d0ef837
Jenny Poon <jenny.poon@live.ca>
2018-11-17 23:46:17 +0000

    added component for paths that do not exist

---
+++ src/Error404.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class ErrorPath extends Component {
+
+  render() {
+    return(
+      <p>
+      404 ERROR: This path does not exist
+      </p>
+    );
+  }
+}
+
+
+export default ErrorPath;
