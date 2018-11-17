c2f7f8f37d962a859cb3e55554c5bbad3ea02097
Jenny Poon <jenny.poon@live.ca>
2018-11-17 23:47:15 +0000

    created component to render search results

---
+++ src/SearchResults.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class SearchResults extends Component {
+
+  render() {
+    return(
+      <p>
+      This is where grid of pictures go
+      </p>
+    );
+  }
+}
+
+
+export default SearchResults;
