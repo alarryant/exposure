2625746bdbb4dc82668578ff81d076ee36431797
Jenny Poon <jenny.poon@live.ca>
2018-11-18 22:29:06 +0000

    removed redirect functions from search bar

--- src/SearchBar.jsx
+++ src/SearchBar.jsx
@@ -1,18 +1,14 @@
 import React, {Component} from 'react';
-import { Redirect } from "react-router-dom"
 
 class SearchBar extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      value: '',
-      redirect: false
+      value: ''
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.setRedirect = this.setRedirect.bind(this);
-    this.renderRedirect = this.renderRedirect.bind(this);
   }
 
   handleChange(event) {
@@ -20,39 +16,11 @@ class SearchBar extends Component {
     console.log("event", event.target.value);
   }
 
-  setRedirect = () => {
-    this.setState({
-      redirect: false
-    })
-  }
-
   handleSubmit(event) {
     event.preventDefault();
     let search_item = this.state.value
     this.props.searchResult(search_item);
-    this.setState({
-      value: '',
-      redirect: true
-    })
-    this.renderRedirect()
   }
-
-  renderRedirect = () => {
-    if (this.state.redirect) {
-      return <Redirect to='/search' />
-      this.setRedirect();
-    }
-  }
-
-  /*
-
-  Background:
-  Tried to set redirect true/false. If true, redirect and then set state back to false again
-
-  QUESTION:
-  1) Redirect to "/search?word=queryword" on submit
-  2) QueryWord will be accepted into Search Result component
-  */
 
   render() {
     return(
@@ -70,7 +38,6 @@ class SearchBar extends Component {
       </label>
         <input type="submit" value="Submit"/>
       </form>
-      {this.renderRedirect()}
       </div>
     );
   }
