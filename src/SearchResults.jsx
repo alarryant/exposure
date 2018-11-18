f316d93ba666752e694e69c8bcb2aa1fdfe8ff67
Jenny Poon <jenny.poon@live.ca>
2018-11-18 03:35:49 +0000

    able to show what the query word if url has search?word=queryWord

--- src/SearchResults.jsx
+++ src/SearchResults.jsx
@@ -1,12 +1,37 @@
 import React, {Component} from 'react';
+import queryString from 'query-string'
 
 class SearchResults extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state ={
+      displayWord: ""
+    }
+
+    this.getQueryWord = this.getQueryWord.bind(this);
+  }
+
+  getQueryWord() {
+    console.log("this props location search: ", this.props.location.search)
+    const values = queryString.parse(this.props.location.search)
+    console.log("values", values.word)
+    this.setState({displayWord: values.word})
+  }
+
+  componentDidMount() {
+    this.getQueryWord()
+  }
 
   render() {
+    console.log("Search results: ", this.props)
     return(
+      <div>
       <p>
+      Results for: {this.state.displayWord}
       This is where grid of pictures go
       </p>
+      </div>
     );
   }
 }
