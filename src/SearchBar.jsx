b18b276bac761a630627c0fde53177044d896dca
Jenny Poon <jenny.poon@live.ca>
2018-11-18 03:57:28 +0000

    tried to redirect state to true/false so redirect would work. Also need to figure out query string

--- src/SearchBar.jsx
+++ src/SearchBar.jsx
@@ -1,29 +1,62 @@
 import React, {Component} from 'react';
-import { NavLink } from "react-router-dom"
+import { Redirect } from "react-router-dom"
 
 class SearchBar extends Component {
-  constructor() {
-    super()
-    this.state = {value: ''};
+  constructor(props) {
+    super(props)
+    this.state = {
+      value: '',
+      redirect: false
+    };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.setRedirect = this.setRedirect.bind(this);
+    this.renderRedirect = this.renderRedirect.bind(this);
   }
 
   handleChange(event) {
     this.setState({value: event.target.value});
-    console.log("event", event.target.value)
+    console.log("event", event.target.value);
+  }
+
+  setRedirect = () => {
+    this.setState({
+      redirect: false
+    })
   }
 
   handleSubmit(event) {
     event.preventDefault();
     let search_item = this.state.value
     this.props.searchResult(search_item);
-    this.setState({value: ''})
+    this.setState({
+      value: '',
+      redirect: true
+    })
+    this.renderRedirect()
   }
+
+  renderRedirect = () => {
+    if (this.state.redirect) {
+      return <Redirect to='/search' />
+      this.setRedirect();
+    }
+  }
+
+  /*
+
+  Background:
+  Tried to set redirect true/false. If true, redirect and then set state back to false again
+
+  QUESTION:
+  1) Redirect to "/search?word=queryword" on submit
+  2) QueryWord will be accepted into Search Result component
+  */
 
   render() {
     return(
+      <div>
       <form onSubmit={ this.handleSubmit }>
        <label>
         Search:
@@ -35,10 +68,10 @@ class SearchBar extends Component {
           onChange={this.handleChange}
         />
       </label>
-      <NavLink to="/search">
         <input type="submit" value="Submit"/>
-      </NavLink>
       </form>
+      {this.renderRedirect()}
+      </div>
     );
   }
 }
