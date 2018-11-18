ddacb8b71f4d03dd2d9f77984e205c5deec24830
Jenny Poon <jenny.poon@live.ca>
2018-11-18 22:29:59 +0000

    removed query string function and used props passed from parent

--- src/SearchResults.jsx
+++ src/SearchResults.jsx
@@ -1,36 +1,15 @@
 import React, {Component} from 'react';
-import queryString from 'query-string'
 
 class SearchResults extends Component {
   constructor(props) {
     super(props);
-
-    this.state ={
-      displayWord: ""
-    }
-
-    this.getQueryWord = this.getQueryWord.bind(this);
   }
 
-  getQueryWord() {
-    console.log("this props location search: ", this.props.location.search)
-    const values = queryString.parse(this.props.location.search)
-    console.log("values", values.word)
-    this.setState({displayWord: values.word})
-  }
-
-  componentDidMount() {
-    this.getQueryWord()
-  }
-
-
-//http://localhost:3000/search?word=wedding : "Results for "Wedding"
   render() {
-    console.log("Search results: ", this.props)
     return(
       <div>
         <p>
-        RESULTS: Images for "{this.state.displayWord}"
+        RESULTS: Images for "{this.props.searchWord}"
         </p>
         <section>
         This is where grid conponent of pictures go
