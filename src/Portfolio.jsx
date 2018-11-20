7f9452e34cc528699569d9563bc161c7e89996d9
Jenny Poon <jenny.poon@live.ca>
2018-11-20 02:28:03 +0000

    first commit for Portfolio component

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -5,14 +5,30 @@ class Portfolio extends React.Component {
 
   constructor(props) {
     super(props);
+
+    // this.displayPortfolio = this.displayPortfolio.bind(this)
   }
 
+  // displayPortofio(images) {
+  //   console.log("displayPortofio", images)
+  //   if (!images || images.length === 0 ) {
+  //     return (<p> Sorry! No photos to share!</p>)
+  //   } else {
+  //     return images.map(function(image) {
+  //       return <img className="searchResult" src={image.src} />
+  //     })
+  //   }
+  // }
+
   render() {
+    console.log("Portfolio", this.props.match)
 
   return (
 
   <div className="profile__portfolio">
     <h1>Steve Irwin's Portfolio</h1>
+    <section className="wrapper">
+    </section>
   </div>
   );
  }
