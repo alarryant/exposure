305e2400e75bbd2b7a9034795ffb46edaf483687
Angela Larryant <angelarryant@gmail.com>
2018-11-21 03:37:02 +0000

    linted profile js

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -7,7 +7,6 @@ import { Link } from 'react-router-dom';
 import axios from 'axios';
 import Url from 'url-parse';
 import EditProfile from './EditProfile';
-// import ProfilePic from '../public/artist_profile.jpg';
 
 class ProfileDesc extends React.Component {
   render() {
