195608d90a1fafe8c365c07ac236543211234e4a
Jenny Poon <jenny.poon@live.ca>
2018-11-21 01:43:36 +0000

    opportunities map through oppcard

---
+++ src/Opportunities.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import "react-tabs/style/react-tabs.css";
+import axios from 'axios';
+import OppCard from './components/OppCard.jsx'
+
+
+class Opportunities extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      opportunities: ''
+    }
+
+    this.displayEvents = this.displayEvents.bind(this)
+  }
+
+  displayEvents(events) {
+    console.log("displayedEvents", events)
+    if (!events || events.length === 0 ) {
+      return (<p> There are currently no postings! Check back later! </p>)
+    } else {
+      return events.map(function(event) {
+        let date = event.event_date.toString().split('T')[0]
+        return (
+          <OppCard event={event} date={date}/>
+          )
+      })
+    }
+  }
+
+  componentDidMount() {
+    axios.get("/api/opportunities").then(res => {
+      this.setState({'opportunities': res.data})
+    })
+}
+
+  render() {
+    return (
+      <section className="opportunities">
+        <h2>Opportunities</h2>
+          { this.displayEvents(this.state.opportunities) }
+      </section>
+    );
+  }
+}
+
+export default Opportunities;
