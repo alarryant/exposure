1089dde8eb2817f8f906e7bb411289c56e3ab80e
Jenny Poon <jenny.poon@live.ca>
2018-11-22 05:07:48 +0000

    added conditions for modal

--- src/components/OppCard.jsx
+++ src/components/OppCard.jsx
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
+import ApplyEvent from './ApplyEvent.jsx'
 
 function OppCard(props) {
-
 const { event } = props
+const user_type_id = parseInt(props.usertype)
 
   return (
     <div className="eventcard">
@@ -11,6 +12,7 @@ const { event } = props
       <hr/>
       <p>{event.description}</p>
       <p>Contact: {event.first_name} {event.last_name}</p>
+      {user_type_id === 1 ? <ApplyEvent date={props.date} event={event.name}/>  : ""}
     </div>
     )
 }
