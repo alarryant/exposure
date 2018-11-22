1b574d3542a45cde50738f9f75106f921c0cdf88
Jenny Poon <jenny.poon@live.ca>
2018-11-22 20:50:00 +0000

    removed id from seeds

--- db/seeds/04_events.js
+++ db/seeds/04_events.js
@@ -1,8 +1,7 @@
 exports.seed = function(knex, Promise) {
   return Promise.all([
     knex('events').insert(
-      { id: 1,
-        name: 'Birthday Party!',
+      { name: 'Birthday Party!',
         description: 'Est Schlitz shoreditch fashion axe. Messenger bag cupidatat Williamsburg sustainable aliqua, artisan duis pickled pitchfork. Semiotics Banksy ad roof party, jean shorts selvage mollit vero consectetur hashtag before they sold out blue bottle qui nihil aute. Aliquip artisan retro squid ullamco.',
         event_date: '2018-10-17',
         price: 150,
@@ -11,8 +10,7 @@ exports.seed = function(knex, Promise) {
         creator_id: 6,
       }),
     knex('events').insert(
-      { id: 2,
-        name: 'Wedding Photographer needed',
+      { name: 'Wedding Photographer needed',
         description: 'Est Schlitz shoreditch fashion axe. Messenger bag cupidatat Williamsburg sustainable aliqua, artisan duis pickled pitchfork. Semiotics Banksy ad roof party, jean shorts selvage mollit vero consectetur hashtag before they sold out blue bottle qui nihil aute. Aliquip artisan retro squid ullamco. Est Schlitz shoreditch fashion axe. Messenger bag cupidatat Williamsburg sustainable aliqua, artisan duis pickled pitchfork. Semiotics Banksy ad roof party, jean shorts selvage mollit vero consectetur hashtag before they sold out blue bottle qui nihil aute. Aliquip artisan retro squid ullamco.',
         event_date: '2019-5-03',
         price: 800,
@@ -21,8 +19,7 @@ exports.seed = function(knex, Promise) {
         creator_id: 7,
       }),
     knex('events').insert(
-      { id: 3,
-        name: 'Graduation Looking for a photographer',
+      { name: 'Graduation Looking for a photographer',
         description: 'Est Schlitz shoreditch fashion axe. Messenger bag cupidatat Williamsburg sustainable aliqua, artisan duis pickled pitchfork. Semiotics Banksy ad roof party, jean shorts selvage mollit vero consectetur hashtag before they sold out blue bottle qui nihil aute. Aliquip artisan retro squid ullamco. Semiotics Banksy ad roof party, jean shorts selvage mollit vero consectetur hashtag before they sold out blue bottle qui nihil aute. Aliquip artisan retro squid ullamco.',
         event_date: '2019-02-10',
         price: 1500,
@@ -31,8 +28,7 @@ exports.seed = function(knex, Promise) {
         creator_id: 8,
       }),
     knex('events').insert(
-      { id: 4,
-        name: 'We need a family portrait',
+      { name: 'We need a family portrait',
         description: 'Est Schlitz shoreditch fashion axe. Messenger bag cupidatat Williamsburg sustainable aliqua, artisan duis pickled pitchfork. Semiotics Banksy ad roof party, jean shorts selvage mollit vero consectetur hashtag before they sold out blue bottle qui nihil aute. Aliquip artisan retro squid ullamco.',
         event_date: '2018-12-27',
         price: 100,
@@ -41,8 +37,7 @@ exports.seed = function(knex, Promise) {
         creator_id: 6,
       }),
     knex('events').insert(
-      { id: 5,
-        name: 'Looking for a portrait photographer',
+      { name: 'Looking for a portrait photographer',
         description: 'Est Schlitz shoreditch fashion axe. Messenger bag cupidatat Williamsburg sustainable aliqua, artisan duis pickled pitchfork. Semiotics Banksy ad roof party, jean shorts selvage mollit vero consectetur hashtag before they sold out blue bottle qui nihil aute. Aliquip artisan retro squid ullamco.',
         event_date: '2019-01-26',
         price: 200,
