62b7aa1e57de73dd82647b4d499587291fc7b4b8
Angela Larryant <angelarryant@gmail.com>
2018-11-27 20:23:15 +0000

    fixed seeds again

--- db/seeds/05_price_packages.js
+++ db/seeds/05_price_packages.js
@@ -3,37 +3,37 @@ exports.seed = function(knex, Promise) {
   return Promise.all([
     knex('price_packages').insert(
       { tier: 1,
-        user_id: 2,
+        user_id: 1,
         price: 80,
         description: `30min to 40min. No changing tent provided. 1 professionally retouched image + 20 to 30 edited digitals`
       }),
     knex('price_packages').insert(
       { tier: 2,
-        user_id: 2,
+        user_id: 1,
         price: 120,
         description: `45min - 1hr. 3 professionally retouched images + 20 to 30 edited digitals.`
       }),
     knex('price_packages').insert(
       { tier: 3,
-        user_id: 2,
+        user_id: 1,
         price: 300,
         description: `2 hrs. 10 professionally retouched images + 40 to 50 edited digitals.`
       }),
     knex('price_packages').insert(
       { tier: 1,
-        user_id: 1,
+        user_id: 2,
         price: 1250,
         description: `Ideal for weddings. 4 hours from the ceremony to the cocktails. Includes: High-resolution photos, a comprehensive consultation session, calibrated images, retouched, each available in colour and black and white, digital images delivered by hyperlink.`
       }),
     knex('price_packages').insert(
       { tier: 2,
-        user_id: 1,
+        user_id: 2,
         price: 1750,
         description: `Ideal for weddings. 6 hours from preparations to cocktails. Includes: High-resolution photos, a comprehensive consultation session, calibrated images, retouched, each available in colour and black and white, digital images delivered by hyperlink.`
       }),
     knex('price_packages').insert(
       { tier: 3,
-        user_id: 1,
+        user_id: 2,
         price: 2750,
         description: `Ideal for weddings. 10 hours from preparations to the first dance. Includes: High-resolution photos, a comprehensive consultation session, calibrated images, retouched, each available in colour and black and white, digital images delivered by hyperlink.`
       }),
